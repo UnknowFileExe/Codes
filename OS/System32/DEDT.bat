04844602dcc3aeb4d46603a1ea9f9741d2aad6e6
UnknowFileExe <73908664+UnknowFileExe@users.noreply.github.com>
2020-11-04 16:05:01 +0100

    Joke OS [WIP]

---
+++ OS/System32/DEDT.bat
@@ -0,0 +1,14 @@
+@echo off
+title DEDT.BAT
+echo eh what ya doing here?
+pause
+echo Nothing here.
+pause
+echo Stop skipping my message!
+pause
+echo Come on leave the Program!
+pause
+echo Im not homee!
+pause
+echo Ok goodbye, next message its will make you leave.
+pause
